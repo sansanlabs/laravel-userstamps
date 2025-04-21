32718777321ba60050db0ae759ada6c08a06d385
Edi Kurniawan <edikurniawan.dev@gmail.com>
2025-04-21 11:27:04 +0700

    style: :art: formatting

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,32 +6,24 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 use VendorName\Skeleton\SkeletonServiceProvider;
 
-class TestCase extends Orchestra
-{
-    protected function setUp(): void
-    {
-        parent::setUp();
+class TestCase extends Orchestra {
+  protected function setUp(): void {
+    parent::setUp();
 
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'VendorName\\Skeleton\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
+    Factory::guessFactoryNamesUsing(fn(string $modelName) => "VendorName\\Skeleton\\Database\\Factories\\" . class_basename($modelName) . "Factory");
+  }
 
-    protected function getPackageProviders($app)
-    {
-        return [
-            SkeletonServiceProvider::class,
-        ];
-    }
+  protected function getPackageProviders($app) {
+    return [SkeletonServiceProvider::class];
+  }
 
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
+  public function getEnvironmentSetUp($app) {
+    config()->set("database.default", "testing");
 
-        /*
+    /*
          foreach (\Illuminate\Support\Facades\File::allFiles(__DIR__ . '/database/migrations') as $migration) {
             (include $migration->getRealPath())->up();
          }
          */
-    }
+  }
 }
