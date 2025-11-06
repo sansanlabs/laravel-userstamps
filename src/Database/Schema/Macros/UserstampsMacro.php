d64b61790c2e1f46e3d7c70197c0af5ea6b97812
Edi Kurniawan <edikurniawan.dev@gmail.com>
2025-11-06 13:20:49 +0700

    fix: :bug: fix foreign when not using morph

--- src/Database/Schema/Macros/UserstampsMacro.php
+++ src/Database/Schema/Macros/UserstampsMacro.php
@@ -14,24 +14,44 @@ class UserstampsMacro implements MacroInterface {
 
   public function addUserIdColumn(Blueprint $table, string $column): void {
     $type = config("userstamps.users_table_id_column_type");
+    $isMorph = config("userstamps.is_using_morph");
+    $usersTable = config("userstamps.users_table_name");
+    $usersIdColumn = config("userstamps.users_table_id_column");
 
     match ($type) {
-      "uuid" => $table->uuid($column)->nullable(),
-      "ulid" => $table->ulid($column)->nullable(),
-      "bigIncrements" => $table->unsignedBigInteger($column)->nullable(),
-      default => $table->unsignedInteger($column)->nullable(),
+      "bigIncrements" => $isMorph
+        ? $table->unsignedBigInteger($column)->nullable()
+        : $table->foreignId($column)->nullable()->constrained(table: $usersTable, column: $usersIdColumn)->nullOnDelete(),
+
+      "uuid" => $isMorph
+        ? $table->uuid($column)->nullable()
+        : $table->foreignUuid($column)->nullable()->constrained(table: $usersTable, column: $usersIdColumn)->nullOnDelete(),
+
+      "ulid" => $isMorph
+        ? $table->ulid($column)->nullable()
+        : $table->foreignUlid($column)->nullable()->constrained(table: $usersTable, column: $usersIdColumn)->nullOnDelete(),
+
+      default => $isMorph
+        ? $table->unsignedInteger($column)->nullable()
+        : (function () use ($table, $column, $usersTable, $usersIdColumn): void {
+          $table->unsignedInteger($column)->nullable();
+          $table->foreign($column)->references($usersIdColumn)->on($usersTable)->nullOnDelete();
+        })(),
     };
   }
 
   private function registerUserstamps(): void {
     Blueprint::macro("userstamps", function (): void {
-      app(UserstampsMacro::class)->addUserIdColumn($this, config("userstamps.created_by_column") . "_id");
+      $createdByColumn = config("userstamps.created_by_column") . "_id";
+      $updatedByColumn = config("userstamps.updated_by_column") . "_id";
+
+      app(UserstampsMacro::class)->addUserIdColumn($this, $createdByColumn);
 
       if (config("userstamps.is_using_morph")) {
         $this->string(config("userstamps.created_by_column") . "_type")->nullable();
       }
 
-      app(UserstampsMacro::class)->addUserIdColumn($this, config("userstamps.updated_by_column") . "_id");
+      app(UserstampsMacro::class)->addUserIdColumn($this, $updatedByColumn);
 
       if (config("userstamps.is_using_morph")) {
         $this->string(config("userstamps.updated_by_column") . "_type")->nullable();
@@ -41,7 +61,9 @@ class UserstampsMacro implements MacroInterface {
 
   private function registerSoftUserstamps(): void {
     Blueprint::macro("softUserstamps", function (): void {
-      app(UserstampsMacro::class)->addUserIdColumn($this, config("userstamps.deleted_by_column") . "_id");
+      $deletedByColumn = config("userstamps.deleted_by_column") . "_id";
+
+      app(UserstampsMacro::class)->addUserIdColumn($this, $deletedByColumn);
 
       if (config("userstamps.is_using_morph")) {
         $this->string(config("userstamps.deleted_by_column") . "_type")->nullable();
@@ -51,7 +73,14 @@ class UserstampsMacro implements MacroInterface {
 
   private function registerDropUserstamps(): void {
     Blueprint::macro("dropUserstamps", function (): void {
-      $columns = [config("userstamps.created_by_column") . "_id", config("userstamps.updated_by_column") . "_id"];
+      $createdByColumn = config("userstamps.created_by_column") . "_id";
+      $updatedByColumn = config("userstamps.updated_by_column") . "_id";
+      $columns = [$createdByColumn, $updatedByColumn];
+
+      if (!config("userstamps.is_using_morph")) {
+        $this->dropForeign([$createdByColumn]);
+        $this->dropForeign([$updatedByColumn]);
+      }
 
       if (config("userstamps.is_using_morph")) {
         $columns[] = config("userstamps.created_by_column") . "_type";
@@ -64,7 +93,12 @@ class UserstampsMacro implements MacroInterface {
 
   private function registerDropSoftUserstamps(): void {
     Blueprint::macro("dropSoftUserstamps", function (): void {
-      $columns = [config("userstamps.deleted_by_column") . "_id"];
+      $deletedByColumn = config("userstamps.deleted_by_column") . "_id";
+      $columns = [$deletedByColumn];
+
+      if (!config("userstamps.is_using_morph")) {
+        $this->dropForeign([$deletedByColumn]);
+      }
 
       if (config("userstamps.is_using_morph")) {
         $columns[] = config("userstamps.deleted_by_column") . "_type";
